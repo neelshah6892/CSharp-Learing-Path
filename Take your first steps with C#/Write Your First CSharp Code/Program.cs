032071a8b71931f6648f00aecbc893c312ce986a
Neel <neelshah6892@gmail.com>
2022-02-23 10:03:46 +0530

    First Lesson

---
+++ Take your first steps with C#/Write Your First CSharp Code/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, World!");
+
+//Challenge
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
+
+//Another Way
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
